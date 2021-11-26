5216d85ee8053c2ea4d2cd12ead08ba4a83abcdd
Mahdi Safarmohammadloo <mahdisml98@gmail.com>
2021-11-26 23:36:30 +0330

    Create childtemplate.tsx

---
+++ childtemplate.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const Div = styled.div`
+
+`;
+
+
+interface Prob {
+  
+}
+export default function Child(props:Prob) {
+  return (
+  
+    <Div>
+      
+    </Div>
+    
+  );
+}
